58d30d194f140d13edb120dba02324c1614064fc
philip <killfilipok@gmail.com>
2018-09-03 19:16:31 +0300

    login_and_forms

--- 01_first_steps/server.go
+++ 01_first_steps/server.go
@@ -1,1 +1,45 @@
 package main
+
+import (
+	"fmt"
+	"html/template"
+	"log"
+	"net/http"
+)
+
+func sayHelloName(w http.ResponseWriter, r *http.Request) {
+	r.ParseForm()
+
+	fmt.Println(r.Form)
+	fmt.Println("path: ", r.URL.Path)
+	fmt.Println("sheme:", r.URL.Scheme)
+	fmt.Println(r.Form["url_long"])
+	for k, v := range r.Form {
+		fmt.Println("key: ", k)
+		fmt.Println("val: ", v)
+	}
+
+	fmt.Fprintf(w, "Hello philip!")
+}
+
+func login(w http.ResponseWriter, r *http.Request) {
+	fmt.Println("method:", r.Method)
+	if r.Method == "GET" {
+		t, _ := template.ParseFiles("login.gtpl")
+		t.Execute(w, nil)
+	} else {
+		r.ParseForm()
+
+		fmt.Println("username:", r.Form["username"])
+		fmt.Println("password:", r.Form["password"])
+	}
+}
+
+func main() {
+	http.HandleFunc("/", sayHelloName)
+	http.HandleFunc("/login", login)
+	err := http.ListenAndServe(":9000", nil)
+	if err != nil {
+		log.Fatal("Server Error: ", err)
+	}
+}
